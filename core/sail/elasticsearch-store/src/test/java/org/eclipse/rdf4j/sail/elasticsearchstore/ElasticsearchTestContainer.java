25e50a84c39de931594ca1b1ff108828c7e99e3a
Håvard M. Ottestad <hmottestad@gmail.com>
2025-06-05 09:54:29 +0200

    Clean up ES indices after each test

--- core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/ElasticsearchTestContainer.java
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/ElasticsearchTestContainer.java
@@ -11,13 +11,22 @@
 package org.eclipse.rdf4j.sail.elasticsearchstore;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.time.Duration;
 import java.util.UUID;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
+import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -51,6 +60,26 @@ public abstract class ElasticsearchTestContainer {
 		transportPort = ES.getMappedPort(9300);
 		clusterName = CLUSTER_NAME;
 		client = new RestHighLevelClient(RestClient.builder(HttpHost.create(ES.getHttpHostAddress())));
+	}
+
+	@AfterEach
+	void cleanIndices() throws Exception {
+		client.indices().refresh(new RefreshRequest("*"), RequestOptions.DEFAULT);
+		try (PreBuiltTransportClient transportClient = new PreBuiltTransportClient(
+				Settings.builder().put("cluster.name", clusterName).build())) {
+			transportClient.addTransportAddress(
+					new TransportAddress(InetAddress.getByName(hostname), transportPort));
+			String[] indices = transportClient.admin()
+					.indices()
+					.getIndex(new GetIndexRequest())
+					.actionGet()
+					.getIndices();
+			for (String index : indices) {
+				if (!index.equals(".geoip_databases")) {
+					client.indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT);
+				}
+			}
+		}
 	}
 
 	@AfterAll
