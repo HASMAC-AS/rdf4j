aa21ae739830f1fd1402166872f8ea40abc559fc
Håvard M. Ottestad <hmottestad@gmail.com>
2025-12-18 08:03:00 +0100

    GH-0000: Format Solr package deprecation note

--- core/sail/solr/src/main/java/org/eclipse/rdf4j/sail/solr/package-info.java
+++ core/sail/solr/src/main/java/org/eclipse/rdf4j/sail/solr/package-info.java
@@ -2,6 +2,6 @@
  * Solr-based search integration.
  *
  * @deprecated since 5.3.0 and scheduled for removal. Use alternative search integrations instead, such as the Lucene or
- *     Elasticsearch Sail implementations.
+ *             Elasticsearch Sail implementations.
  */
 package org.eclipse.rdf4j.sail.solr;
